03b863b448a9cd269639000bd0fc83e834aefb4f
Lakshay Gupta <gupta15.lakshay@gmail.com>
2024-06-13 21:32:50 +0530

    chore: Update sorting algorithm buttons styling

--- src/SortingVisualizer/Sorting.jsx
+++ src/SortingVisualizer/Sorting.jsx
@@ -70,10 +70,10 @@ function Sorting() {
         </div>
 
         <div className="flex justify-evenly text-white">
-        <button onClick={resetArr} className="text-white">Generate New Array</button>
-        <button onClick={MergeSort} className="text-white">Merge Sort</button>
-        <button onClick={QuickSort}>Quick Sort</button>
-        <button onClick={BubbleSort}>Bubble Sort</button>
+        <button onClick={resetArr} className="px-8 z-30 py-4 bg-rose-400 rounded-md text-white relative font-semibold font-sans after:-z-20 after:absolute after:h-1 after:w-1 after:bg-rose-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#be123c;] hover:[text-shadow:2px_2px_2px_#fda4af] text-2xl">Generate New Array</button>
+        <button onClick={MergeSort} className="px-8 z-30 py-4 bg-rose-400 rounded-md text-white relative font-semibold font-sans after:-z-20 after:absolute after:h-1 after:w-1 after:bg-rose-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#be123c;] hover:[text-shadow:2px_2px_2px_#fda4af] text-2xl">Merge Sort</button>
+        <button onClick={QuickSort} className="px-8 z-30 py-4 bg-rose-400 rounded-md text-white relative font-semibold font-sans after:-z-20 after:absolute after:h-1 after:w-1 after:bg-rose-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#be123c;] hover:[text-shadow:2px_2px_2px_#fda4af] text-2xl">Quick Sort</button>
+        <button onClick={BubbleSort} className="px-8 z-30 py-4 bg-rose-400 rounded-md text-white relative font-semibold font-sans after:-z-20 after:absolute after:h-1 after:w-1 after:bg-rose-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#be123c;] hover:[text-shadow:2px_2px_2px_#fda4af] text-2xl">Bubble Sort</button>
         </div>
 
       </div>
